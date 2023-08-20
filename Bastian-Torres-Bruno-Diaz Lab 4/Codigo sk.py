2500ab4200d9b3d7ae8aeab2e121f1b5a8115872
bruno.diaz04@alumnos.ucn.cl <bruno.diaz04@alumnos.ucn.cl>
2023-08-20 00:16:13 -0400

    se me olvido actualizar github

--- Bastian-Torres-Bruno-Diaz Lab 4/Codigo sk.py
+++ Bastian-Torres-Bruno-Diaz Lab 4/Codigo sk.py
@@ -25,7 +25,7 @@ data["Velocidad"]=Dataframe_velocidades.values #agrego la serie que tiene propie
 
 #--------------------------------------------------------------------------------------------------------------------
 
-def Encontrar_coordenada_vecina():
+def Encontrar_coordenada_vecina(data_filtrado,Coordenadas_X_Y,arbol,df):
 
     data_filtrado["indice matriz"] = [i for i in range(len(data_filtrado))]
 
@@ -45,24 +45,26 @@ def Encontrar_coordenada_vecina():
             #calculo de la distancia
             distances, indices = arbol.query(Coordenda_a_evaluar, k=len(indice_vecino)+1)
             inicial=distances[0]
-            data["sk"].loc[int(indices_filtrados)]= np.mean([i for i in (distances) if i!=inicial ])
+            df["sk"].loc[int(indices_filtrados)]= np.mean([i for i in (distances) if i!=inicial ])
 
 #--------------------------------------------------------------------------------------------------------------------
+def principal(df):
+    for frame in df["Frame"].unique():
 
-for frame in data["Frame"]:
+        data_filtrado=df[df["Frame"]==frame].copy() #filtro de data por el frame que se encuentre en el for, .copy para no alterar al dataframe original
+        #cuando agregue los indices de la matriz
+        Coordenadas_X_Y=data_filtrado[["X","Y"]].values #creo matriz con valores x e y del dataframe filtrado cada fila es una coordenada xy por persona
+        arbol=KDTree(Coordenadas_X_Y) #crea arbol con opciones
+        Encontrar_coordenada_vecina(data_filtrado,Coordenadas_X_Y,arbol,df)
 
-    data_filtrado=data[data["Frame"]==frame].copy() #filtro de data por el frame que se encuentre en el for, .copy para no alterar al dataframe original
-    #cuando agregue los indices de la matriz
-    Coordenadas_X_Y=data_filtrado[["X","Y"]].values #creo matriz con valores x e y del dataframe filtrado cada fila es una coordenada xy por persona
-    arbol=KDTree(Coordenadas_X_Y) #crea arbol con opciones
-    Encontrar_coordenada_vecina()
-
+principal(data)
 #hacer scarter plot sf en y velocidad en el y
 print("Promedio sk:",data["sk"].mean())
 print("Desviacion sk:",data["sk"].std())
 print("Promedio Velocidad:",data["Velocidad"].mean())
 print("Desviacion Velocidad:",data["Velocidad"].std())
 #--------------------------------------------------------------------------------------------------------------------
+
 
 tabla_experimento1=Dataframe_velocidades.groupby("# PersID").agg(np.mean)
 fig, ax = plt.subplots()
@@ -84,15 +86,59 @@ plt.xlabel('sk')
 plt.ylabel('Velocidad')
 plt.grid(True)
 
-#obtencion de grafico cajas y bigotes primeras 10 personas del primer experimento
-fig, ax = plt.subplots()
-ax.boxplot([data[(data["# PersID"]==i)]["Velocidad"]  for i in range(1,11)])
-ax.set_xlabel('Personas')
-ax.set_ylabel('Velocidad')
-ax.set_title('Boxplot por persona experimento 01', loc='center')
 
+
+"---------------------------------------------------------------------------------------------------"
+
+data2=pd.read_csv("UNI_CORR_500_06.txt", skiprows=3,sep="\t") #names= cuando la tabla no tiene un encabezado
+data2["sk"]=0
+
+#--------------------------------------------------------------------------------------------------------------------
+
+#Velocidad primer txt
+def Obtener_velocidad(datax):
+    return (((datax["X"].diff(periods=1))**2+(datax["Y"].diff(periods=1))**2)**(1/2))/(1/25)
+
+Dataframe_velocidades2 = data2.groupby("# PersID").apply(Obtener_velocidad) #dataframe_velocidad se convirtio en una serie
+
+data2["Velocidad"]=Dataframe_velocidades2.values #agrego la serie que tiene propiedad index y value al dataframe original
+#en este caso values contiene un total de las filas de datos
+
+
+principal(data2)
+#hacer scarter plot sf en y velocidad en el y
+print("Promedio sk:",data2["sk"].mean())
+print("Desviacion sk:",data2["sk"].std())
+print("Promedio Velocidad:",data2["Velocidad"].mean())
+print("Desviacion Velocidad:",data2["Velocidad"].std())
+
+#--------------------------------------------------------------------------------------------------------------------
+
+
+tabla_experimento2=Dataframe_velocidades2.groupby("# PersID").agg(np.mean)
+fig, ax = plt.subplots()
+ax.hist(tabla_experimento2,bins=15, color='green', edgecolor='black')
+ax.set_xlabel('Velocidad')
+ax.set_ylabel('Frecuencia')
+ax.set_title('Histograma de Velocidad experimento 1', loc='center')
+
+
+sk = data2['sk']
+velocidad = data2['Velocidad']
+
+data2=data2.dropna(subset=['Velocidad']) #elimino valores none
+
+plt.figure(figsize=(10, 6))
+plt.scatter(sk,velocidad)  # alpha controla la transparencia de los puntos
+plt.title('Scatter Plot entre sk y Velocidad')
+plt.xlabel('sk')
+plt.ylabel('Velocidad')
+plt.grid(True)
+
+"-----------------------------------------------------------------------------------------------------------------"
 
 fin_memoria = psutil.virtual_memory().used;           fin_tiempo = time.time()
+plt.show()
 plt.show()
 plt.show()
 plt.show()
